e4b15854a62af63202b59c9576c63f662e4da7fe
RAKSHA MODI <76099016+rakshamodi@users.noreply.github.com>
2022-08-17 17:56:06 +0530

    Commit

---
+++ SpringEmailClient/src/main/java/com/mail/SpringEmailClient/SpringEmailClientApplication.java
@@ -0,0 +1,26 @@
+package com.mail.SpringEmailClient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import javax.mail.MessagingException;
+
+@SpringBootApplication
+public class SpringEmailClientApplication {
+
+	@Autowired
+	private EmailSenderService senderService;
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmailClientApplication.class, args);
+	}
+	@EventListener(ApplicationReadyEvent.class)
+	public void triggerMail() throws MessagingException {
+		senderService.sendSimpleEmail("brahmandastronomy@gmail.com",
+				"This is email subject",
+				"This is email body");
+
+	}
+}
